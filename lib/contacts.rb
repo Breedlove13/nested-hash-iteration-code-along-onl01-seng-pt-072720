048198f278ac38cb3a59b08f784f92c4816696ca
Brian Breedlove <b.breedlove@outlook.com>
2020-08-02 01:16:16 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,6 +19,7 @@ def remove_strawberry(contacts)
 contacts.each do |person, contact_details_hash|
 if person == "Freddy Mercury"
   contact_details_hash.each do |attribute, data|
+    if attribut == :favorite_ice_cream_flavors
       end
     end 
    end 
